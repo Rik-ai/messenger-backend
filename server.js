44c6a0ec828be576327e09b9ea8e6f604145e37b
Ivan <ivan.lakhanin@gmail.com>
2020-09-18 15:10:03 +0300

    connect to mongo DB

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //importing
 const express = require('express')
-
+const mongoose = require('mongoose')
 
 //app config
 const app = express();
@@ -9,6 +9,12 @@ const port = process.env.PORT || 9000;
 //middleware
 
 //DB config
+const connection_url = 'mongodb+srv://admin:G8E7b3xFx29TTZ5@cluster0.2hvei.mongodb.net/messengerDB?retryWrites=true&w=majority'
+mongoose.connect(connection_url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTodology: true
+})
 
 //????
 
